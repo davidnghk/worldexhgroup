ae697f178fd290eae8b53fc3959dfbf0e3fb706e
David Ng <david.ng.hk@gmail.com>
2016-11-11 03:05:10 +0000

    Release version

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     root to: 'visitors#index'
     devise_for :users
     resources :users
+    get "notice" => "pages#notice"
     get "india" => "pages#portfolio-india"
     get "brazil" => "pages#portfolio-brazil"
     get "iran" => "pages#portfolio-iran"
